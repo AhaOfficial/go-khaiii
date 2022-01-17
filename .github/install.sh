310779b54571170c81820becf2105b7755827a4f
Jonghyeok Lee <jh.lee@a-ha.io>
2022-01-17 15:39:32 +0900

    update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -5,6 +5,9 @@ cd `dirname ${BASH_SOURCE}`
 BASE=$(pwd)
 KHAIII_LIBC_DIR=$(pwd)/build/khaiii-libc
 
+echo "[BASE] ${BASE}"
+echo "[KHAIII_LIBC_DIR] ${KHAIII_LIBC_DIR}"
+
 # Build and Install Khaiii Library for C
 cd ${KHAIII_LIBC_DIR}
 qmake khaiii-libc.pro
