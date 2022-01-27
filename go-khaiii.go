b25a26387701c9482fd0c3f6a72106825aa74086
Jonghyeok Lee <jh.lee@a-ha.io>
2022-01-27 20:06:41 +0900

    add customized functions

--- go-khaiii.go
+++ go-khaiii.go
@@ -6,7 +6,10 @@ package go_khaiii
 import "C"
 import (
 	"errors"
+	"fmt"
+	"regexp"
 	"strings"
+	"unicode/utf8"
 	"unsafe"
 )
 
@@ -34,7 +37,7 @@ func (m *Model) Create(rsc_dir string, opt_str string) error {
 	return nil
 }
 
-func (m *Model) Parse(line string) ([][]string, error) {
+func (m *Model) Parse(line string, opt ...map[string]string) ([][]string, error) {
 	var morphs_result [][]string
 
 	c_line := C.CString(line)
@@ -57,6 +60,12 @@ func (m *Model) Parse(line string) ([][]string, error) {
 		morphs_result = append(morphs_result, []string{lex, tag})
 	}
 
+	if len(opt) > 0 {
+		if _, ok := opt[0]["--no-consonant"]; ok {
+			morphs_result = removeConsonantMorphs(morphs_result, line)
+		}
+	}
+
 	return morphs_result, nil
 }
 
@@ -67,4 +76,69 @@ func (m *Model) Destroy() error {
 	} else {
 		return nil
 	}
+}
+
+func removeConsonantMorphs(morphs [][]string, org string) [][]string {
+	var morphs_result [][]string
+	var keep_word []string
+
+	include_consonant := false
+	consonant_regexp, _ := regexp.Compile("^[ㄱ-ㅎ]")
+	for _, word := range morphs {
+		if consonant_regexp.MatchString(word[0]) {
+			include_consonant = true
+			break
+		}
+	}
+	if !include_consonant {
+		return morphs
+	}
+
+	temp := strings.ReplaceAll(org, " ", "")
+	for _, word := range morphs {
+		index := -1
+		if keep_word == nil {
+			index = strings.IndexAny(temp, word[0])
+		} else {
+			left_word, right_word := combineWord(keep_word[0], word[0])
+			index = strings.IndexAny(temp, fmt.Sprintf("%s%s", left_word, right_word))
+
+			keep_word = []string{left_word, keep_word[1]}
+			word = []string{right_word, word[1]}
+		}
+
+		if index == 0 {
+			temp = temp[len(word[0]):]
+			if keep_word != nil {
+				morphs_result = append(morphs_result, keep_word)
+				keep_word = nil
+			}
+			if len(word[0]) > 0 {
+				morphs_result = append(morphs_result, word)
+			}
+		} else {
+			keep_word = word
+		}
+	}
+	return morphs_result
+}
+
+func combineWord(a_str string, b_str string) (string, string) {
+	a_str_tail_rune := []rune(a_str)[utf8.RuneCountInString(a_str)-1]
+	a_str_head := a_str[:len(a_str)-len(string(a_str_tail_rune))]
+
+	b_str_head_rune := []rune(b_str)[0]
+	b_str_tail := b_str[len(string(b_str_head_rune)):]
+
+	middle := string(combineChar(a_str_tail_rune, b_str_head_rune))
+
+	return fmt.Sprintf("%s%s", a_str_head, middle), b_str_tail
+}
+
+func combineChar(cho_jung rune, jong rune) rune {
+	temp := cho_jung - rune('가')
+	cho := temp / 588
+	jung := (temp - (cho * 588)) / 28
+	jong = jong - rune('ㄱ') + 1
+	return (588 * rune(cho)) + (28 * rune(jung)) + jong + rune('가')
 }
--- go-khaiii_test.go
+++ go-khaiii_test.go
@@ -1,7 +1,9 @@
 package go_khaiii
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -32,6 +34,57 @@ func TestParse(t *testing.T) {
 		t.Error("Parse Error!")
 		panic(err)
 	} else {
+		fmt.Println(result)
+		err = model.Destroy()
+		if err != nil {
+			t.Error("Destroy Error!")
+			panic(err)
+		}
+	}
+}
+
+func TestParseCustom_without_consonant(t *testing.T) {
+	var model Model
+	sentence := "나는 회사에 갔습니다"
+	err := model.Create("", "")
+	if err != nil {
+		t.Error("Create Error!")
+		panic(err)
+	}
+	result, err := model.Parse(sentence, map[string]string{"--no-consonant": ""})
+	if err != nil {
+		t.Error("Parse Error!")
+		panic(err)
+	} else {
+		fmt.Println(result)
+		err = model.Destroy()
+		if err != nil {
+			t.Error("Destroy Error!")
+			panic(err)
+		}
+	}
+}
+
+func TestParseCustom(t *testing.T) {
+	var model Model
+	sentence := "나는 회사에 간다"
+	err := model.Create("", "")
+	if err != nil {
+		t.Error("Create Error!")
+		panic(err)
+	}
+	result, err := model.Parse(sentence, map[string]string{"--no-consonant": ""})
+	if err != nil {
+		t.Error("Parse Error!")
+		panic(err)
+	} else {
+		var resultSentence string
+		for _, morphs := range result {
+			resultSentence += morphs[0]
+		}
+		if resultSentence != strings.ReplaceAll(sentence, " ", "") {
+			panic(errors.New("Result is Incorrect!"))
+		}
 		fmt.Println(result)
 		err = model.Destroy()
 		if err != nil {
